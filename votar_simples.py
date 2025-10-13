05ce469f21516b8de9ac6914b9d9a7e01f8ffe0d
Thalles Quintiliano <thalles.quintiliano@gpnet.com.br>
2025-10-13 20:48:17 -0300

    aula 03 modify

--- votar_simples.py
+++ votar_simples.py
@@ -1,10 +1,14 @@
 # Verificação de Voto - Exemplo Simples
 
-# Entrada da idade
-idade = 18
+# F-STRING (f""): Insere variáveis no texto usando {}
+# Exemplo: print(f"Olá {nome}") em vez de print("Olá " + nome)
+
+# Entrada do nome e idade pelo usuário
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
 
 # Verificação simples
 if idade >= 16:
-    print("PODE VOTAR")
+    print(f"{nome}, você PODE VOTAR")
 else:
-    print("NÃO PODE VOTAR")
+    print(f"{nome}, você NÃO PODE VOTAR")
